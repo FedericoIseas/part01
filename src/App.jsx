e05062624929f257342d2de963802d82d91f7289
FedericoIseas <federicoiseas@gmail.com>
2025-02-26 13:01:55 -0300

    pre-excercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-const App = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Federico' age={26+3}/>
+      <Hello name= {name} age= {age}/>
+    </>
   )
 }
 
